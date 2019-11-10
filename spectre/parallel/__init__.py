6c4b919bd009932838ab5bc1bc7bddaa5c35e6ba
Heerozh <heeroz@gmail.com>
2019-11-10 20:39:13 +0800

    ParallelGroupBy class

--- spectre/parallel/__init__.py
+++ spectre/parallel/__init__.py
@@ -1,3 +1,3 @@
 from .algorithmic import (
-    groupby
+    ParallelGroupBy
 )
--- spectre/parallel/algorithmic.py
+++ spectre/parallel/algorithmic.py
@@ -1,28 +1,45 @@
-from typing import Tuple
+from typing import Tuple, List
 import torch
 import numpy as np
 
 
-def groupby(data: torch.Tensor, key: torch.Tensor) -> Tuple[torch.Tensor, list, dict]:
+# ----------- group -----------
+
+class ParallelGroupBy:
     """Fast parallel group by"""
-    n = data.shape[0]
-    # sort by key
-    relative_key = key + torch.arange(0, n, device=data.device).float() / n
-    sorted_keys, sorted_key_indices = torch.sort(relative_key)
-    # get group
-    diff = sorted_keys[1:].int() - sorted_keys[:-1].int()
-    boundary = (diff.nonzero(as_tuple=True)[0] + 1).cpu().tolist()
-    boundary = np.array([0] + boundary + [n])
-    width = max(boundary[1:] - boundary[:-1])
-    # split
-    groups = len(boundary) - 1
-    ret = data.new_zeros((groups, width))
-    inverse_indices, key_to_group = [], {}
-    for start, end, i in zip(boundary[:-1], boundary[1:], range(groups)):
-        ret[i, 0:(end-start)] = data[sorted_key_indices[start:end]]
-        key_to_group[int(sorted_keys[start].item())] = i
-        inverse_indices.append(sorted_key_indices[start:end])
-    return ret, inverse_indices, key_to_group
+    def __init__(self, key: torch.Tensor):
+        n = key.shape[0]
+        # sort by key
+        relative_key = key + torch.arange(0, n, device=key.device).double() / n  # todo test double float performance difference
+        sorted_keys, sorted_indices = torch.sort(relative_key)
+        # get group boundary
+        diff = sorted_keys[1:].int() - sorted_keys[:-1].int()
+        boundary = (diff.nonzero(as_tuple=True)[0] + 1).cpu().tolist()
+        boundary = np.array([0] + boundary + [n])
+        # get inverse indices
+        width = max(boundary[1:] - boundary[:-1])
+        groups = len(boundary) - 1
+        inverse_indices = key.new_full((groups, width), n+1)
+        for start, end, i in zip(boundary[:-1], boundary[1:], range(groups)):
+            inverse_indices[i, 0:(end-start)] = sorted_indices[start:end]
+        inverse_indices = torch.sort(inverse_indices.flatten())[1][:n]
+        # class members
+        self._boundary = boundary
+        self._sorted_indices = sorted_indices
+        self._inverse_indices = inverse_indices
+        self._width = width
+        self._groups = groups
+
+    def split(self, data: torch.Tensor) -> torch.Tensor:
+        # split
+        ret = data.new_zeros((self._groups, self._width))
+        for start, end, i in zip(self._boundary[:-1], self._boundary[1:], range(self._groups)):
+            ret[i, 0:(end - start)] = data[self._sorted_indices[start:end]]
+        return ret
+
+    def revert(self, split_data: torch.Tensor) -> torch.Tensor:
+        return torch.take(split_data, self._inverse_indices)
+
 
 
 
