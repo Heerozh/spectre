e0e1d49535b2677d3f2dc90b657a8b54b28a851a
Heerozh <heeroz@gmail.com>
2019-11-18 15:18:57 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="spectre",
+    author='Zhang Jianhao',
+    author_email='heeroz@gmail.com',
+    description='GPU-accelerated Parallel quantitative trading library',
+    long_description=open('README.md', encoding='utf-8').read(),
+    license='Apache 2.0',
+    keywords='quantitative analysis backtesting parallel algorithmic trading',
+    url='https://github.com/Heerozh/spectre',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+
+    version="0.08",
+    packages=['spectre', 'spectre.factors', 'spectre.parallel'],
+)
