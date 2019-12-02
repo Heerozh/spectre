aed3f1e32ae547d4d2a39f55d4f573c24b2b6cd0
Heerozh <heeroz@gmail.com>
2019-12-02 23:30:21 +0800

    in architecture...

--- tests/test_trading_algorithm.py
+++ tests/test_trading_algorithm.py
@@ -1,29 +1,46 @@
 import unittest
 import spectre
+from os.path import dirname
+
+data_dir = dirname(__file__) + '/data/'
 
 
 class TestTradingAlgorithm(unittest.TestCase):
 
     def test_one_engine_algorithm(self):
-        class OneEngineAlgo(spectre.trading.CustomAlgorithm):
-            def initialize(self, blotter):
+        class OneEngineAlg(spectre.trading.CustomAlgorithm):
+            def initialize(self):
                 engine = self.get_factor_engine()
                 ma5 = spectre.factors.MA(5)
                 engine.add(ma5, 'ma5')
                 engine.set_filter(ma5.top(5))
 
+                self.schedule(spectre.trading.event.EveryTick(self.rebalance))
+
                 self.blotter.set_commission()
                 self.blotter.set_slippage()
 
-            def handle_data(self, data):
+            def rebalance(self, data):
                 weight = data.ma5 / data.ma5.sum()
                 self.order_to_percent(data.index, weight)
 
             def analyze(self):
                 pass
 
+        loader = spectre.factors.CsvDirLoader(
+            data_dir + '/daily/', 'AAPL',
+            ohlcv=('uOpen', 'uHigh', 'uLow', 'uClose', 'uVolume'),
+            index_col='date', parse_dates=True,
+        )
+        blotter = spectre.trading.SimulationBlotter()
+        evt_mgr = spectre.trading.SimulationEventManager()
+        alg = OneEngineAlg(blotter, loader)
+        evt_mgr.subscribe(alg)
+        evt_mgr.run()
+
+
     def test_two_engine_algorithm(self):
-        class TwoEngineAlgo(spectre.trading.CustomAlgorithm):
+        class TwoEngineAlg(spectre.trading.CustomAlgorithm):
             def initialize(self, blotter):
                 engine_5mins = self.get_factor_engine('5mins')
                 engine_daily = self.get_factor_engine('daily')
