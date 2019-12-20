1492a318c0af27d4e3a4e2a07fff3e53771f8012
Heerozh <heeroz@gmail.com>
2019-12-20 22:38:11 +0800

    plotting with same scale

--- spectre/factors/plotting.py
+++ spectre/factors/plotting.py
@@ -61,43 +61,45 @@ def plot_quantile_and_cumulative_returns(factor_data, mean_ret):
         subplot_titles=['Quantile Return', 'Portfolio cumulative returns'],
     )
 
+    mean_ret = mean_ret * 10000
     for i, factor in enumerate(factors):
         row = i + 1
         weight_col = (factor, 'factor_weight')
         weighted = factor_data['Returns'].multiply(factor_data[weight_col], axis=0)
         factor_return = weighted.groupby(level='date').sum()
         for j, period in enumerate(periods):
-            y = mean_ret.loc[:, (factor, period, 'mean')] * 10000
-            err_y = mean_ret.loc[:, (factor, period, 'sem')] * 10000
+            y = mean_ret.loc[:, (factor, period, 'mean')]
+            err_y = mean_ret.loc[:, (factor, period, 'sem')]
             fig.add_trace(go.Bar(
                 x=x, y=y, error_y=dict(type='data', array=err_y, thickness=0.2),
-                **quantile_styles[period]
+                yaxis='y1', **quantile_styles[period]
             ), row=row, col=1)
             quantile_styles[period]['showlegend'] = False
 
             cum_ret = factor_return[period].resample('b' + period).mean().dropna()
             cum_ret = (cum_ret + 1).cumprod() * 100 - 100
             fig.add_trace(go.Scatter(
-                x=cum_ret.index, y=cum_ret.values,  **cumulative_styles[period]
+                x=cum_ret.index, y=cum_ret.values, yaxis='y2', **cumulative_styles[period]
             ), row=row, col=2)
 
         fig.update_xaxes(type="category", row=row, col=1)
-        fig.update_yaxes(title_text=factor, row=row, col=1)
 
         fig.add_shape(go.layout.Shape(
             type="line", line=dict(width=1),
-            y0=1, y1=1, x0=factor_return.index[0], x1=factor_return.index[-1],
+            y0=0, y1=0, x0=factor_return.index[0], x1=factor_return.index[-1],
         ), row=row, col=2)
 
         weight_diff = factor_data[weight_col].unstack(level=[1]).diff()
-        to = weight_diff.abs().sum(axis=1)*100
+        to = weight_diff.abs().sum(axis=1) * 100
         resample = int(len(to) / 64)
         to = to.fillna(0).rolling(resample).mean()[::resample]
         fig.add_trace(go.Bar(x=to.index, y=to.values, **turnover_styles),
                       secondary_y=True, row=row, col=2)
         turnover_styles['showlegend'] = False
 
+        fig.update_yaxes(title_text=factor, row=row, col=1, matches='y1')
         fig.update_yaxes(row=row, col=2, ticksuffix='%')
+        fig.update_yaxes(row=row, col=2, secondary_y=False, matches='y2')
 
     fig.update_layout(height=300 * rows, barmode='group', bargap=0.5, margin={'t': 50})
     fig.show()
