9b29286d27ef4d1d6debc842e4ac4a26207c4179
Heerozh <heeroz@gmail.com>
2020-06-10 16:07:18 +0800

    ENH: add pop_passed arg for calendar

--- spectre/trading/calendar.py
+++ spectre/trading/calendar.py
@@ -22,7 +22,7 @@ class Calendar:
         self.events = defaultdict(list)
         self.timezone = None
 
-    def build(self, end: str, daily_events: Dict[str, str], tz='UTC', freq='B'):
+    def build(self, end: str, daily_events: Dict[str, str], tz='UTC', freq='B', pop_passed=True):
         """ build("2020", {'Open': '9:00:00', 'Close': '15:00:00'}) """
         self.timezone = tz
         days = pd.date_range(pd.Timestamp.now(tz=tz).normalize(), pd.Timestamp(end, tz=tz),
@@ -34,8 +34,9 @@ class Calendar:
         self.events = {name: [day + pd.Timedelta(time) for day in days]
                        for name, time in daily_events.items()}
 
-        for k, _ in self.events.items():
-            self.pop_passed(k)
+        if pop_passed:
+            for k, _ in self.events.items():
+                self.pop_passed(k)
 
     def add_event(self, event: str, datetime: pd.Timestamp):
         self.events[event].append(datetime)
@@ -87,7 +88,7 @@ class CNCalendar(Calendar):
         *pd.date_range('2020-10-01', '2020-10-08', freq='D'),
     ]
 
-    def __init__(self):
+    def __init__(self, pop_passed=True):
         super().__init__()
         timezone = 'Asia/Shanghai'
         assert pd.Timestamp.now(self.timezone).year <= CNCalendar.closed[-1].year
@@ -101,6 +102,6 @@ class CNCalendar(Calendar):
                 'Close': '15:00:00',
                 'DayEnd': '23:59:59'
             },
-            tz=timezone)
+            tz=timezone, pop_passed=pop_passed)
         for d in CNCalendar.closed:
             self.set_as_holiday(d.tz_localize(timezone))
