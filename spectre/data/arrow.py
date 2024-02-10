38854c782f27aeafd5259c9854d84ea8d2f5217c
Heerozh <heeroz@gmail.com>
2024-02-11 01:33:51 +0800

    ENH: ArrowLoader add filter function, for pre-screen data in pandas

--- spectre/data/arrow.py
+++ spectre/data/arrow.py
@@ -31,6 +31,7 @@ class ArrowLoader(DataLoader):
         super().__init__(path, ohlcv, adjustments)
         self.keep_in_memory = keep_in_memory
         self._cache = None
+        self._filter = None
 
     @classmethod
     def _last_modified(cls, file_path) -> float:
@@ -60,6 +61,10 @@ class ArrowLoader(DataLoader):
         meta.adjustments[:2] = source.adjustments
         meta.to_feather(save_to + '.meta')
 
+    def filter(self, func):
+        self._filter = func
+        self._cache = None
+
     def _load(self) -> pd.DataFrame:
         if self._cache is not None:
             return self._cache
@@ -70,6 +75,9 @@ class ArrowLoader(DataLoader):
         else:
             df = pd.read_feather(self._path)
         df.set_index(['date', 'asset'], inplace=True)
+
+        if self._filter is not None:
+            df = self._filter(df)
 
         if self.keep_in_memory:
             self._cache = df
